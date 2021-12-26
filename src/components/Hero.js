b78200ad11c6ff672c2d9102bc7aeb5c44f046ad
VicKLyo <gsbgvikistarbg@abv.bg>
2021-12-26 14:09:07 +0200

    Initial Commit

---
+++ src/components/Hero.js
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
+
+import './Hero.css'
+
+
+
+const Hero = () => {
+    return (
+     <div className="hero-container">
+        <div className="name-wrapper"> <h1><span>Viktor</span> Yordanov</h1></div>
+        <div className="under-name-wrapper">Front End Web Developer
+                <div className="icon-container">
+                    <a target='_blank' href="https://github.com/VicKLyo"> <BsGithub className='icon' > </BsGithub></a> 
+                    <a target='_blank' href="https://www.linkedin.com/in/viktor-yordanov-b01370221/y"><BsLinkedin className='icon'></BsLinkedin> </a>
+                </div>
+                </div>
+       </div>
+    
+        
+    )
+}
+
+export default Hero
---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import './Navbar.css'
+import { useState, useEffect } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { IconContext } from 'react-icons/lib'
+import { Link } from 'react-router-dom'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
+
+
+
+
+
+const Navbar = () => {
+const [click, setClick] = useState(false)
+const handleClick = () => setClick(!click)
+const closeMobileMenu = () => setClick(false)
+
+
+    return (
+       
+        <>
+             <div className="navbar-container">
+             <div className="name">Viktor Yordanov</div>
+
+                 <div className="navbar">
+                 <div className="menu-icon" onClick={handleClick}>
+                            {click ? <FaTimes /> : <FaBars/>}
+                        </div>
+                    <ul   className={click ? 'nav-menu active' : 'nav-menu'}>
+                        
+                        <li className="nav-item" > <Link to='/' className='nav-links' onClick={closeMobileMenu}> Home </Link> </li>
+                        <li className="nav-item" > <Link to='/about' className='nav-links' onClick={closeMobileMenu}> About </Link></li>
+                        <li className="nav-item" > <Link to='/projects' className='nav-links' onClick={closeMobileMenu}> Projects </Link></li>
+                        <li className="nav-item" > <Link to='/contact' className='nav-links' onClick={closeMobileMenu}> Contact </Link></li>
+                        <li className="nav-item"> <a className='cv' href="">CV</a> </li>
+                  </ul>
+                </div>
+                
+                <div className="icon-container">
+                <a target='_blank' href="https://github.com/VicKLyo"> <BsGithub className='icon' > </BsGithub></a> 
+                <a target='_blank' href="https://www.linkedin.com/in/viktor-yordanov-b01370221/y"><BsLinkedin className='icon'></BsLinkedin> </a> 
+                </div>
+                
+              
+                </div>
+        </>
+    )
+}
+
+export default Navbar
