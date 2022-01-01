ced3a81196512513c261dfde98b6d65bd2116569
VicKLyo <gsbgvikistarbg@abv.bg>
2022-01-01 21:04:27 +0200

    Added Projects Section

---
+++ src/components/Projects.js
@@ -0,0 +1,22 @@
+import './Projects.css'
+
+
+const Projects = () => {
+    return (
+        <div className='projects-container'>
+            <div className="projects-title-wrapper">
+                <h2>Projects</h2>
+            </div>
+            <div className="project-wrapper">
+                <div className="project-box"></div>
+                <div className="project-box"></div>
+                <div className="project-box"></div>
+                <div className="project-box"></div>
+                <div className="project-box"></div>
+                <div className="project-box"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Projects
