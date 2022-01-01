3807a9c145f8b6e49e1cd2b80b73fc0c9efe8e52
VicKLyo <gsbgvikistarbg@abv.bg>
2022-01-01 14:38:27 +0200

    Changed about text

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import './About.css'
 import {ReactComponent as ReactImage} from './programming.svg';
 
+
 const About = () => {
     return (
         <div className="about-container">
@@ -9,12 +10,11 @@ const About = () => {
             <ReactImage className="about-image"/>
             </div>
             <div className="about-content-wrapper">
-
             <div className="about-title-wrapper">
                 <h2>About</h2>
             </div>
             <div className="about-text-wrapper">
-                <p>I'm an IT student at NBU with love for the Front End. I also love playing sports, working out and living a healthy lifestyle. Passionate, hard-working and hungry to learn more every day.</p>
+                <p>I'm an IT student at New Bulgarian University with love for the Front End. I also enjoy playing sports, working out and living a healthy lifestyle. Passionate, hard-working and hungry to learn more every day.</p>
             </div>
             </div>
         </div>
