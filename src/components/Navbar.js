74966ddb0e669b6b7182d27642c281087fa28b4a
VicKLyo <gsbgvikistarbg@abv.bg>
2022-01-01 14:39:01 +0200

    Added skills to navbar and changed font sizes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,6 +29,7 @@ const closeMobileMenu = () => setClick(false)
                         <li className="nav-item" > <Link to='/' className='nav-links' onClick={closeMobileMenu}> Home </Link> </li>
                         <li className="nav-item" > <Link to='/about' className='nav-links' onClick={closeMobileMenu}> About </Link></li>
                         <li className="nav-item" > <Link to='/projects' className='nav-links' onClick={closeMobileMenu}> Projects </Link></li>
+                        <li className="nav-item" > <Link to='/skills' className='nav-links' onClick={closeMobileMenu}> Skills </Link></li>
                         <li className="nav-item" > <Link to='/contact' className='nav-links' onClick={closeMobileMenu}> Contact </Link></li>
                         <li className="nav-item"> <a className='cv' href="">CV</a> </li>
                   </ul>
