7193931fbd7c097a5200367f1de3e61be60ee59e
VicKLyo <gsbgvikistarbg@abv.bg>
2022-01-01 14:34:58 +0200

    Added skills section

---
+++ src/components/Skills.js
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom'
+import {ReactComponent as ReactImage} from './hero.svg';
+import { AiFillHtml5 } from 'react-icons/ai'
+import { SiJavascript, SiTailwindcss, SiBootstrap } from 'react-icons/si'
+import { DiReact, DiGit, DiCss3, DiSass } from 'react-icons/di'
+import { BsFillBootstrapFill } from 'react-icons/bs'
+import './Skills.css'
+
+const Skills = () => {
+    return (
+        <div className="skills-container">
+           <div className="top-half"></div>
+           <div className="skills-content">
+               <div className="skills-title-wrapper">
+               <h2>Skills</h2>
+               </div>
+               <div className="skills-icon-wrapper">
+               <AiFillHtml5 className='skills-icons' />
+               <DiCss3 className='skills-icons' />
+               <SiJavascript className='skills-icons' /> 
+               <SiTailwindcss  className='skills-icons' color='mediumTurquoise' /> 
+               <DiReact className='skills-icons' />
+               <DiGit className='skills-icons' />
+               <DiSass className='skills-icons' />
+               <SiBootstrap className='skills-icons' /> 
+
+               </div>
+           </div>
+           <div className="bottom-half"></div>
+       </div>
+    )
+}
+
+export default Skills
