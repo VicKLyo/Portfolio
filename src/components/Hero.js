35fadfee2cff9ca88994c85c11d2b7f0ccc4c322
VicKLyo <gsbgvikistarbg@abv.bg>
2022-01-06 20:27:04 +0200

    Small changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,8 @@ const Hero = () => {
     return (
      <div className="hero-container">
         <div className="name-wrapper"> <h1><span>Viktor</span> Yordanov</h1></div>
-        <div className="under-name-wrapper">Front End Web Developer
+        <div className="under-name-wrapper">
+            <h2>Front End Web Developer</h2>
                 <div className="icon-container">
                     <a target='_blank' href="https://github.com/VicKLyo"> <BsGithub className='icon' > </BsGithub></a> 
                     <a target='_blank' href="https://www.linkedin.com/in/viktor-yordanov-b01370221/y"><BsLinkedin className='icon'></BsLinkedin> </a>
