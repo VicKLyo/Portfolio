12a62eeb814be50747c61b74eeeacd6bb3789122
VicKLyo <gsbgvikistarbg@abv.bg>
2022-01-12 09:18:49 +0200

    Image and text are now props

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,63 +1,81 @@
 import './Projects.css'
 import { BsGithub } from 'react-icons/bs'
+import image2 from './Screenshot_1.png'
+import { useState } from 'react';
+
+
+const ProjectPreview = ({ image, text }) => {
+  
+    return (
+        <div className='projects-container'>
+            <div
+                className="project-preview">
+                <img src={image} alt="" />
+                    <div className="hover-container">
+                        <p>{text}</p>
+                    </div>
+                </div>
+        </div>
+    );
+};
 
 
 
 const Projects = () => {
+
     return (
-        <div className='projects-container'>
+        <div id='projects' className='projects-container'>
             <div className="projects-title-wrapper">
                 <h2>Projects</h2>
             </div>
             <div className="project-wrapper">
 
                 <div className="boxes-wrapper">
-
-                <div className="box">
-                    <div className="project-preview"></div>
+                <div className="box" >
+                     <ProjectPreview text={"My description"} image={image2} /> 
                     <div className="live-git">
-                        <span>Live</span>
-                        <BsGithub/>
+                       <a href="" target="_blank"><span className='live-btn'>Live</span></a> 
+                      <a href="" target="_blank"><BsGithub className='projects-icon'/></a>  
                     </div>
                 </div>
 
                 <div className="box">
-                    <div className="project-preview"></div>
+                <ProjectPreview text={"My description"} image={image2} /> 
                     <div className="live-git">
-                    <span>Live</span>
-                        <BsGithub/>
+                    <a href="" target="_blank"><span className='live-btn'>Live</span></a> 
+                    <a href="" target="_blank"><BsGithub className='projects-icon'/></a>  
                     </div>
                 </div>
 
                 <div className="box">
-                    <div className="project-preview"></div>
+                <ProjectPreview text={"My description"} image={image2} /> 
                     <div className="live-git">
-                    <span>Live</span>
-                        <BsGithub/>
+                    <a href="" target="_blank"><span className='live-btn'>Live</span></a> 
+                    <a href="" target="_blank"><BsGithub className='projects-icon'/></a>  
                     </div>
                 </div>
                     
                 <div className="box">
-                    <div className="project-preview"></div>
+                <ProjectPreview text={"My description"} image={image2} /> 
                     <div className="live-git">
-                    <span>Live</span>
-                        <BsGithub/>
+                    <a href="" target="_blank"><span className='live-btn'>Live</span></a> 
+                    <a href="" target="_blank"><BsGithub className='projects-icon'/></a>  
                     </div>
                 </div>
 
                 <div className="box">
-                    <div className="project-preview"></div>
+                <ProjectPreview text={"My description"} image={image2} /> 
                     <div  className="live-git">
-                    <span>Live</span>
-                        <BsGithub/>
+                    <a href="" target="_blank"> <span className='live-btn'>Live</span> </a> 
+                    <a href="" target="_blank"><BsGithub className='projects-icon'/></a>  
                     </div>
                 </div>
 
                 <div className="box">
-                    <div className="project-preview"></div>
+                <ProjectPreview text={"My description"} image={image2} /> 
                     <div className="live-git">
-                    <span>Live</span>
-                        <BsGithub/>
+                    <a href="" target="_blank"><span className='live-btn'>Live</span></a> 
+                      <a href="" target="_blank"><BsGithub className='projects-icon'/></a>  
                     </div>
                 </div>
                 
